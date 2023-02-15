e716694b7438fb88aa507fe604b1a5f604ad5440
EOussama <oussama_gtacj@live.fr>
2023-02-15 13:01:09 +0100

    Angular template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scala_front';
+export class AppComponent implements OnInit {
+
+  //#region Properties
+
+  users: Array<any>;
+
+  //#endregion
+
+  //#region Lifecycle
+
+  constructor() {
+    this.users = [];
+  }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  //#endregion
+
+  //#region Event handlers
+
+  onRefresh(): void {
+    this.loadUsers();
+  }
+
+  //#endregion
+
+  //#region Methods
+
+  private loadUsers(): void {
+    this.users = [
+      { id: 1, first_name: 'Oussama', last_name: 'Essamadi', age: 24 },
+      { id: 2, first_name: 'John', last_name: 'Doe', age: 47 }
+    ];
+  }
+
+  //#endregion
 }
